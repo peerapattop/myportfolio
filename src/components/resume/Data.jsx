49cfbacc665c273bc49e3252eb8eea2223003226
peerapattop <peerapatza401@gmail.com>
2024-03-08 23:33:55 +0700

    Experience components

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,52 @@
+const Data = [
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2023 - present",
+    title: "Bachelor of Industrial Technology",
+    desc: "Technology Electronics Computer at King Mongkut's University of Technology North Bangkok",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2021 - 2023",
+    title: "High Vocational Certificate",
+    desc: "Telecommunications technology at Samut Sakhon Technical College ",
+  },
+  {
+    id: 3,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2018 - 2021",
+    title: "Vocational Certificate",
+    desc: "Electronics at Samut Sakhon Technical College",
+  },
+  {
+    id: 4,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2024 - present",
+    title: "Developer",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+  {
+    id: 5,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2021 - 2022",
+    title: "Technician Engineer Intern, True",
+    desc: "Troubleshoot internet outages,Install internet cable",
+  },
+  {
+    id: 6,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2020 - 2021",
+    title: "Refrigeration Engineer Intern, Thai Union",
+    desc: "Check the air conditioner,Repair damaged refrigeration",
+  },
+];
+
+export default Data;
