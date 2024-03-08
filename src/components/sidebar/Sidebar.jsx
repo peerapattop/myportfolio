fe45dfad1e23d5eada36e28a6209cfb17f0a27e6
peerapattop <peerapatza401@gmail.com>
2024-03-08 13:42:40 +0700

    sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import Logo from "../../assets/logo.svg";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className="aside">
+      <a href="#home" className="nva__logo">
+        <img src={Logo} alt="" />
+      </a>
 
-export default Sidebar
+      <nav className="nav">
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="icon-home"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="about" className="nav__link">
+                <i className="icon-user-following"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="icon-briefcase"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#resume" className="nav__link">
+                <i className="icon-graduation"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="icon-layers"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="icon-bubble"></i>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+
+      <div className="nav__footer">
+        <span className="copyright"> &copy; 2023 - 2024.</span>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
