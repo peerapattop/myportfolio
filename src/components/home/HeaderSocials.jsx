800df0df8932fc6d1f9289d33f78b44524a7fe11
peerapattop <peerapatza401@gmail.com>
2024-03-08 15:12:18 +0700

    home component

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.linkedin.com/in/pirapattop/" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/peerapattop" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="https://www.facebook.com/peerapattopten/" className="home__social-link" target='_blank'>
+        <i class="fa-brands fa-facebook"></i>
+        </a>
+
+        <a href="" className="home__social-link" target='_blank'>
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Home.css'
+import Me from "../../assets/avatar-1.svg"
+import HeaderSocials from './HeaderSocials'
+import ScrollDown from './ScrollDown'
+import Shapes from './Shapes'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home container' id="home">
+      <div className='intro'>
+        <img src={Me} className='home__img'/>
+        <h1 className='home__name'>Pirapat Tangpanyawai</h1>
+        <span className='home__education'>I'am a Front-End Developer</span>
+
+        <HeaderSocials/>
+
+        <a href='#contact' className='btn'>Hire Me</a>
+
+        <ScrollDown/>
+
+        <Shapes/>
+      </div>
+    </section>
   )
 }
 
