a05433c3a786fff81c4402fcad63b0648f91dfcc
peerapattop <peerapatza401@gmail.com>
2024-03-09 14:29:03 +0700

    portfolio components

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,19 @@
+
+const Menu = [
+  {
+    id: 1,
+    image: "https://img2.pic.in.th/pic/Screenshot_20240223_213954cut.jpeg",
+    title: "Mobile Exchange Of Things",
+    category: "MobileApplication",
+    link : "https://github.com/peerapattop/swapitem-user-2-firebase"
+  },
+  {
+    id: 2,
+    image: "https://img5.pic.in.th/file/secure-sv1/web-1.png",
+    title: "Portfolio Website",
+    category: "WebApplication",
+    link : "https://github.com/peerapattop/myportfolio"
+  },
+];
+
+export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Portfolio.css'
+import Menu from './Menu'
 
 const Portfolio = () => {
+  const [items,setItem] = useState(Menu);
+  const filterItem = (categoryItem) => {
+    const updatedItems = Menu.filter((curElem) => {
+      return curElem.category === categoryItem;
+    });
+    setItem(updatedItems);
+  };
   return (
-    <div>Portfolio</div>
+    <section className="work container section" id='work'>
+      <h2 className='section__title'>Projects</h2>
+
+      <div className="work__filters">
+        <span className="work__item" onClick={()=> setItem(Menu)}>Everything</span>
+        <span className="work__item" onClick={()=> filterItem("WebApplication")}>Web Application</span>
+        <span className="work__item" onClick={()=> filterItem("MobileApplication")}>Mobile Application</span>
+      </div>
+
+      <div className='work__container grid'>
+        {items.map((elem)=>{
+          const{id,image,title,category,link} = elem;
+          return (
+            <div className="work__card" key={id}>
+             <div className='work__thumbnail'>
+              <img src={image} alt="" className='work__img' />
+                <div className='work__mask'></div>
+             </div>
+              
+              <span className='work__category'>{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href={link} className="work__button">
+                <i className='icon-link work__button-icon'></i>
+              </a>
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
