498a698905bde79e11d054c123a3dabcc64ca8aa
peerapattop <peerapatza401@gmail.com>
2024-03-10 19:05:49 +0700

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <section className='home container' id="home">
       <div className='intro'>
         <img src='https://img2.pic.in.th/pic/1707410239098.jpeg' className='home__img'/>
-        <h1 className='home__name'>Pirapat Tangpanyawai</h1>
+        <h1 className='home__name'>Pirapat Tangpanyawaid</h1>
         <span className='home__education'>I'am a Front-End Developer</span>
 
         <HeaderSocials/>
