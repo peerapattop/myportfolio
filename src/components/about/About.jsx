92c92ea8a942a73eda8d9dccbdc52ef24a443ef2
peerapattop <peerapatza401@gmail.com>
2024-03-10 19:20:47 +0700

    update data

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,9 @@ const About = () => {
         
         <div className='about__data grid'>
           <div className="about__info">
-            <p className="about__description">My name is Pirapat Tangpanyawai. My nickname is Top. I am 22 years old. I live in Bangkok. Now I'm a student at King Mongkut's University of Technology North Bangkok.</p>
+            <p className="about__description">My name is Pirapat Tangpanyawai. My nickname is Top. I am 22 years old. I live in Bangkok. Now I'm a student at King Mongkut's University of Technology North Bangkok. In the
+              future, I hope I will be a good programmer. After graduating bachelor's degree, I plan to be a
+              professional programmer in Web Developer and Mobile Developer.</p>
             <a href="https://drive.google.com/file/d/1e1ASwGs-NP_jKdyJ9Wth8OtNDTAhnZvK/view?usp=sharing" className="btn">Check Resume</a>
             <a href="https://drive.google.com/file/d/1QB8VQXP-nPbJPlaRrIylySWO5O_kifwb/view?usp=sharing" className="btn">Check Transcript</a>          
           </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,11 +10,11 @@ const Home = () => {
       <div className='intro'>
         <img src='https://img2.pic.in.th/pic/1707410239098.jpeg' className='home__img'/>
         <h1 className='home__name'>Pirapat Tangpanyawai</h1>
-        <span className='home__education'>I'am a Front-End Developer</span>
+        <span className='home__education'>I'am a Front-End Developer and Mobile Developer</span>
 
         <HeaderSocials/>
 
-        <a href='#contact' className='btn'>Hire Me</a>
+        <a href='#contact' className='btn'>Contact Me</a>
 
         <ScrollDown/>
 
