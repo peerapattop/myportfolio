601e1949eeebe6496c237392f544fbd5add40af9
peerapattop <peerapatza401@gmail.com>
2024-03-11 16:07:42 +0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,8 +11,8 @@ const About = () => {
         
         <div className='about__data grid'>
           <div className="about__info">
-            <p className="about__description">My name is Pirapat Tangpanyawai. My nickname is Top. I am 22 years old. I live in Bangkok. Now I'm a student at King Mongkut's University of Technology North Bangkok. In the
-              future, I hope I will be a good programmer. After graduating bachelor's degree, I plan to be a
+            <p className="about__description">&nbsp;&nbsp;&nbsp;My name is Pirapat Tangpanyawai. My nickname is Top. I am 22 years old. I live in Bangkok. Now I'm a student at King Mongkut's University of Technology North Bangkok. 
+                 <br/>&nbsp;&nbsp;&nbsp;&nbsp; In the future, I hope I will be a good programmer. After graduating bachelor's degree, I plan to be a
               professional programmer in Web Developer and Mobile Developer.</p>
             <a href="https://drive.google.com/file/d/1e1ASwGs-NP_jKdyJ9Wth8OtNDTAhnZvK/view?usp=sharing" className="btn">Check Resume</a>
             <a href="https://drive.google.com/file/d/1QB8VQXP-nPbJPlaRrIylySWO5O_kifwb/view?usp=sharing" className="btn">Check Transcript</a>          
