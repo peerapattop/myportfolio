d7a4364d67356817c02bba7bdc4e80e04c368ee7
peerapattop <peerapatza401@gmail.com>
2024-04-03 20:01:46 +0700

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <section className='home container' id="home">
       <div className='intro'>
-        <img src='https://img2.pic.in.th/pic/1707410239098.jpeg' className='home__img'/>
+        <img src='https://img2.pic.in.th/pic/1707410239098.jpeg' className='home__img' alt='mypicture'/>
         <h1 className='home__name'>Pirapat Tangpanyawai</h1>
         <span className='home__education'>I'am a Front-End Developer and Mobile Developer</span>
 
