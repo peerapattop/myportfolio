7977f69cba5868b6cbd2821947caead1176474c7
peerapattop <peerapatza401@gmail.com>
2024-04-03 19:57:55 +0700

    Update HeaderSocials.jsx

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const HeaderSocials = () => {
   return (
     <div className="home__socials">
-        <a href="https://www.linkedin.com/in/pirapattop/" className="home__social-link" target='_blank'>
-            <i class="fa-brands fa-linkedin"></i>
-        </a>
+      <a
+        href="https://www.linkedin.com/in/pirapattop/"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
 
-        <a href="https://github.com/peerapattop" className="home__social-link" target='_blank'>
-            <i class="fa-brands fa-github"></i>
-        </a>
+      <a
+        href="https://github.com/peerapattop"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fa-brands fa-github"></i>
+      </a>
 
-        <a href="https://www.facebook.com/peerapattopten/" className="home__social-link" target='_blank'>
+      <a
+        href="https://www.facebook.com/peerapattopten/"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i class="fa-brands fa-facebook"></i>
-        </a>
+      </a>
 
-        <a href="tel:092-098-7484" className="home__social-link" target='_blank'>
+      <a href="tel:092-098-7484" className="home__social-link" target="_blank"  rel="noreferrer">
         <i class="fa-solid fa-phone"></i>
-        </a>
-
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
