fa64a6899b692e2af835a176c1882fc68586fe87
peerapattop <peerapatza401@gmail.com>
2024-08-04 21:51:00 +0700

    update portfolio

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -23,6 +23,14 @@ const Menu = [
     details: "Todo List",
     category: "WebApplication",
     link : "https://todo-app-omega-beryl.vercel.app/"
+  },
+  {
+    id: 4,
+    image: "https://img2.pic.in.th/pic/-2024-08-04-214943.png",
+    title: "Food Delivery",
+    details: "Todo List",
+    category: "WebApplication",
+    link : "https://food-delivery-frontend-azyk.onrender.com/"
   }
 ];
 
